f02b60a7364c61a27997e2e673f4a31e21a90417
Roman Vesely <crazko@gmail.com>
2015-04-07 09:37:28 +0200

    added changes for lesson 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,23 @@
-var oButton = new sap.ui.commons.Button('myButton');
+var settings = {
+	text: 'My Button',
+	icon: 'sap-icon://popup-window'
+}
 
-oButton.placeAt('content');
+var oButton = new sap.ui.commons.Button('myButton', settings);
+
+oButton.attachPress(openConfirm);
+
+function openConfirm() {
+	sap.ui.commons.MessageBox.confirm('Some message text', fnCallback, 'Alert title');
+}
+
+function fnCallback(result) {
+	alert('Result is ' + result);
+}
+
+
+var oShell = new sap.m.Shell("mainShell", {
+	app: oButton
+});
+
+oShell.placeAt('content');
