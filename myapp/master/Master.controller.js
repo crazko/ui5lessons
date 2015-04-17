ee760d91e1cb31623e135f7d539d1dd8b9d38345
Roman Veselý <crazko@gmail.com>
2015-04-17 08:45:28 +0200

    Update Master.controller.js

--- myapp/master/Master.controller.js
+++ myapp/master/Master.controller.js
@@ -1,4 +1,20 @@
 sap.ui.controller('myapp.master.Master', {
+  
+  onInit: function() {
+		var oModel = new sap.ui.model.json.JSONModel('model/inbox.json');
+		this.getView().setModel(oModel);
+	},
 
+	onExit: function() {
+
+	},
+
+	onAfterRendering: function() {
+
+	},
+
+	onAfterRendering: function() {
+
+	}
 
 });
